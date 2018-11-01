417e3a11f44cc562594b06b9c4d5a37de9277fde
Crysfel Villa <crysfel@bleext.com>
2018-11-01 15:16:31 -0500

    Rendering latest transactions grouped by day

--- spa/src/components/Panel/index.js
+++ spa/src/components/Panel/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Panel({ children, title }) {
   return (
-    <div className="bg-white p-8 border-t-4 border-orange">
+    <div className="bg-white py-8 px-4 border-t-4 border-orange">
       {title && <h3 className="mb-4">{title}</h3> }
       {children}
     </div>
--- spa/src/pages/Dashboard/index.js
+++ spa/src/pages/Dashboard/index.js
@@ -1,9 +1,54 @@
 import React, { Fragment } from 'react';
+import { useMappedState } from 'redux-react-hook';
+import { format } from 'date-fns'
+import classNames from 'classnames';
+import { getLatestGroupedByDate } from 'store/modules/transactions/selectors';
+
+import Panel from 'components/Panel';
+
+const today = format(new Date(), 'YYYY-MM-DD');
+const mapState = state => ({
+  latest: getLatestGroupedByDate(state),
+});
 
 export default function Dashboard() {
+  const { latest } = useMappedState(mapState);
+
   return (
     <Fragment>
       <h1 className="text-grey-darkest mb-8">Dashboard</h1>
+      <Panel title="Latest transactions">
+        <ul className="m-0 p-0">
+        { latest.map(group => <TransactionGroup key= {group.date} transactions={group} />) }
+        </ul>
+      </Panel>
     </Fragment>
+  );
+}
+
+function TransactionGroup({ transactions }) {
+  const css = classNames("list-reset flex border-l-4 p-4", {
+    'border-green': transactions.total > 0,
+    'border-red': transactions.total < 0
+  });
+
+  return (
+    <li className={css}>
+      <GroupTitle group={transactions} />
+      <strong>{transactions.total}</strong>
+    </li>
+  );
+}
+
+function GroupTitle({ group }) {
+  const date = new Date(group.date);
+  const formated = format(new Date(date), 'ddd DD, MMM YYYY');
+  const day = format(new Date(date), 'dddd');
+
+  return (
+    <span className="flex-1">
+      <span className="block mb-1">{group.date === today ? 'Today' : day }</span>
+      <small className="block text-grey">{formated} &bull; {group.items.length} Transactions</small>
+    </span>
   );
 }
