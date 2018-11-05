c555d5b753840128979dad805cd21083f678ad08
Crysfel Villa <crysfel@bleext.com>
2018-11-04 20:41:07 -0600

    Commenting additional fields

--- spa/src/pages/Transactions/Add.js
+++ spa/src/pages/Transactions/Add.js
@@ -47,8 +47,8 @@ export default function AddTransaction({ isExpense }) {
         <TextField label="Amount" placeholder="00.00" value={state.amount} onChange={(event) => setValue(event, 'amount')} type="number" />
         <TextField label="Tags" placeholder={tags} value={state.tags} onChange={(event) => setValue(event, 'tags')} />
         <TextField label="Description" placeholder={description} value={state.description} onChange={(event) => setValue(event, 'description')} multiline />
-        { isExpense && <TextField label="Store" placeholder="Target, Walmart, Amazon" value={state.store} onChange={(event) => setValue(event, 'store')} /> }
-        <TextField label="When" value={state.date} onChange={(event) => setValue(event, 'date')} type="date" />
+        {/* { isExpense && <TextField label="Store" placeholder="Target, Walmart, Amazon" value={state.store} onChange={(event) => setValue(event, 'store')} /> }
+        <TextField label="When" value={state.date} onChange={(event) => setValue(event, 'date')} type="date" /> */}
         <Button onClick={saveTransaction}>Save</Button>
       </Panel>
     </Fragment>
